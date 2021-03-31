8eb4f49a336c6cd5d04e380ba42b065e5d572c44
IvDin <evan.baldin@gmail.com>
2021-03-31 12:01:19 +0300

    marker builder added

--- lib/model/map_list.dart
+++ lib/model/map_list.dart
@@ -1,202 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-//icons for lists
-BitmapDescriptor museumIcon = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta);
-BitmapDescriptor religionIcon = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed);
-BitmapDescriptor militaryIcon = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen);
+//
+// extending the google map marker to add more information
+// to it
+class MapMarker extends Marker {
+  final String name;
+  final String type;
+  final int page;
 
-//lists of markers
-List<Marker> museumList = [
-  Marker(
-    markerId: MarkerId("1"),
-    position: LatLng(60.082630, 30.031255),
-    infoWindow: InfoWindow(title: "Музей «Шалаш»"),
-    icon: museumIcon,
-  ),
-  Marker(
-    markerId: MarkerId("2"),
-    position: LatLng(60.079895, 29.976921),
-    infoWindow: InfoWindow(title: "Музей «Сарай»"),
-    icon: museumIcon,
-  ),
-  Marker(
-    markerId: MarkerId("3"),
-    position: LatLng(60.113087, 29.971866),
-    infoWindow: InfoWindow(title: "Музей «Сестрорецкий рубеж»"),
-    icon: museumIcon,
-  ),
-  Marker(
-    markerId: MarkerId("4"),
-    position: LatLng(60.155804, 29.896731),
-    infoWindow: InfoWindow(title: "Музей-усадьба И. Е. Репина «Пенаты»"),
-    icon: museumIcon,
-  ),
-  Marker(
-    markerId: MarkerId("5"),
-    position: LatLng(60.184223, 29.789947),
-    infoWindow: InfoWindow(title: "Музей «Келломяки – Комарово»"),
-    icon: museumIcon,
-  ),
-  Marker(
-    markerId: MarkerId("6"),
-    position: LatLng(60.191687, 29.703867),
-    infoWindow: InfoWindow(title: "Музей ретро-автомобилей"),
-    icon: museumIcon,
-  ),
-  Marker(
-    markerId: MarkerId("7"),
-    position: LatLng(60.122125, 30.156579),
-    infoWindow: InfoWindow(title: "Малый Садовый сквер"),
-    icon: museumIcon,
-  ),
-  Marker(
-    markerId: MarkerId("8"),
-    position: LatLng(60.167917, 29.860979),
-    infoWindow: InfoWindow(title: "Репино"),
-    icon: museumIcon,
-  ),
-];
-List<Marker> religionList = [
-  Marker(
-    markerId: MarkerId("1"),
-    position: LatLng(60.200121, 29.688443),
-    infoWindow: InfoWindow(title: "Храм Покрова Божией матери"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("2"),
-    position: LatLng(60.103617, 29.971210),
-    infoWindow: InfoWindow(title: "Храм святых апостолов Петра и Павла"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("3"),
-    position: LatLng(60.171635, 29.869983),
-    infoWindow: InfoWindow(title: "Храм-часовня Преображения Господня"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("4"),
-    position: LatLng(60.192398, 29.706222),
-    infoWindow: InfoWindow(title: "Храм Казанской иконы Божией матери"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("5"),
-    position: LatLng(60.125738, 30.160218),
-    infoWindow: InfoWindow(title: "Храм Серафима Саровского"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("6"),
-    position: LatLng(60.184690, 29.813569),
-    infoWindow: InfoWindow(title: "Церковь Святого духа и новая церковь"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("7"),
-    position: LatLng(60.123718, 30.122091),
-    infoWindow: InfoWindow(title: "Церковь Петра и Павла"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("8"),
-    position: LatLng(60.101304, 30.209253),
-    infoWindow: InfoWindow(title: "Церковь Святого Николая Чудотворца"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("9"),
-    position: LatLng(60.195830, 29.706692),
-    infoWindow: InfoWindow(title: "Лютеранская Кирха Преображения Господня"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("10"),
-    position: LatLng(60.125417, 30.147307),
-    infoWindow: InfoWindow(title: "Часовня Серафима Саровского"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("11"),
-    position: LatLng(60.089561, 29.965577),
-    infoWindow: InfoWindow(title: "Часовня Николая Чудотворца"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("12"),
-    position: LatLng(60.196749, 29.531765),
-    infoWindow: InfoWindow(title: "Могила Крестовской в поселке Молодежное. Марьина гора"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("13"),
-    position: LatLng(60.213689, 29.730898),
-    infoWindow: InfoWindow(title: "Зеленогорское кладбище. Могила Герценштейна"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("14"),
-    position: LatLng(60.095171, 30.190954),
-    infoWindow: InfoWindow(title: "Левашовское мемориальное кладбище"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("15"),
-    position: LatLng(60.034729, 29.986544),
-    infoWindow: InfoWindow(title: "Кладбище в Горской"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("16"),
-    position: LatLng(60.066926, 29.974975),
-    infoWindow: InfoWindow(title: "Тарховское кладбище"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("17"),
-    position: LatLng(60.203942, 29.799603),
-    infoWindow: InfoWindow(title: "Комаровское кладбище"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("18"),
-    position: LatLng(60.108471, 29.971036),
-    infoWindow: InfoWindow(title: "Сестрорецкое кладбище"),
-    icon: religionIcon,
-  ),
-  Marker(
-    markerId: MarkerId("19"),
-    position: LatLng(60.204525, 29.799806),
-    infoWindow: InfoWindow(title: "Могила Анны Ахматовой"),
-    icon: religionIcon,
-  ),
-];
-List<Marker> militaryList = [
-  Marker(
-    markerId: MarkerId("1"),
-    position: LatLng(60.030205, 30.015883),
-    infoWindow: InfoWindow(title: "Аэродром в Горской"),
-    icon: militaryIcon,
-  ),
-  Marker(
-    markerId: MarkerId("2"),
-    position: LatLng(60.090518, 30.195451),
-    infoWindow: InfoWindow(title: "Военный аэродром в Левашово"),
-    icon: militaryIcon,
-  ),
-  Marker(
-    markerId: MarkerId("3"),
-    position: LatLng(60.099072, 30.213370),
-    infoWindow: InfoWindow(title: "Военный мемориал и памятник дважды Герою СССР А. Т. Карпова"),
-    icon: militaryIcon,
-  ),
-  Marker(
-    markerId: MarkerId("4"),
-    position: LatLng(60.184712, 29.818013),
-    infoWindow: InfoWindow(title: "Братская могила воинов Советской Армии/погибших "
-        "в боях Советско-финской и Великой Отечественной войны 1941-1945гг"),
-    icon: militaryIcon,
-  ),
+  MapMarker(this.name, this.type, this.page, {MarkerId id, lat, lng, onTap, icon})
+      : super(
+    markerId: id,
+    consumeTapEvents: false,
+    position: LatLng(
+      lat,
+      lng,
+    ),
+    icon: icon,
+    infoWindow: InfoWindow(title: name, snippet: type),
+    onTap: onTap,
+  );
+}
+
+List<Map<String, dynamic>> locations = [
+  {
+    "Location_Number": "4",
+    "Location_Name": "Дамба",
+    "Location_Type": "museum",
+    "Location_Page": 120,
+    "coordinates": [29.865693, 60.028998]
+  },
+  {
+    "Location_Number": "3",
+    "Location_Name": "Аэродром",
+    "Location_Type": "military",
+    "Location_Page": 124,
+    "coordinates": [30.015883, 60.030205]
+  },
+  {
+    "Location_Number": "2",
+    "Location_Name": "Кладбище",
+    "Location_Type": "museum",
+    "Location_Page": 126,
+    "coordinates": [29.986544, 60.034729]
+  },
+  {
+    "Location_Number": "1",
+    "Location_Name": "Горская",
+    "Location_Type": "military",
+    "Location_Page": 118,
+    "coordinates": [29.980015, 60.042399]
+  },
+  {
+    "Location_Number": "13",
+    "Location_Name": "Зеленогорское кладбище. Могила Герценштейна",
+    "Location_Type": "military",
+    "Location_Page": 102,
+    "coordinates": [29.730898, 60.213689]
+  }
 ];
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,22 +1,67 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:kurortny_guide_flutter/model/map_list.dart';
-import 'package:kurortny_guide_flutter/pages/place_page.dart';
-import 'package:kurortny_guide_flutter/widgets/map_filter.dart';
 import 'package:kurortny_guide_flutter/widgets/map_sheet.dart';
 
-Completer<GoogleMapController> _controller = Completer();
-
 class MapPage extends StatefulWidget {
-
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
+  GoogleMapController mapController;
+  Set<MapMarker> markersList = new Set();
+  
+  BitmapDescriptor markerIcon(String type) {
+    if(type == 'museum') {
+      return BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure);
+    } else if(type == 'military') {
+      return BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed);
+    } else {
+      return BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet);
+    }
+  }
 
-  bool isMuseumChecked = true;
+  void _addMarkers() {
+    locations.forEach((Map<String, dynamic> location) {
+      final MapMarker marker = MapMarker(
+          location['Location_Name'],
+          location['Location_Type'],
+          location['Location_Page'],
+          id: MarkerId(location['Location_Number']),
+          lat: location['coordinates'][1],
+          lng: location['coordinates'][0],
+          icon: markerIcon(location['Location_Type']),
+          onTap:()=> showModalBottomSheet(
+            context: context,
+            backgroundColor: Colors.transparent,
+            builder: (BuildContext context){
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: Expanded(
+                    child: MapSheet(
+                      name: location['Location_Name'],
+                      page: location['Location_Page'],
+                      coordinates: LatLng(
+                        location['coordinates'][1],
+                        location['coordinates'][0],
+                      ),
+                    ),
+                  )
+                ),
+              );
+            },
+          ),
+      );
+      markersList.add(marker);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,254 +69,54 @@ class _MapPageState extends State<MapPage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text("Карта"),
+        title: Text('Карта'),
         actions: [
           IconButton(
+            //onPressed: onPressed,
             icon: Icon(Icons.settings, color: Colors.cyan,),
-            onPressed: ()=> showDialog(
-                context: context,
-                builder: (BuildContext buildContext){
-                  return MapFilter();
-                }
-            ),
           ),
         ],
       ),
-      body: Stack(
-        children: <Widget>[_googleMaps(context)],
-      ),
+      body: GoogleMap(
+          initialCameraPosition:
+          CameraPosition(target: LatLng(60.0702,30.1120), zoom: 9),
+          onMapCreated: _onMapCreated,
+          myLocationEnabled: true,
+          compassEnabled: false,
+          myLocationButtonEnabled: true,
+          zoomControlsEnabled: true,
+          mapToolbarEnabled: true,
+          indoorViewEnabled: true,
+          mapType: MapType.normal,
+          markers: markersList.toSet()),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+    _addMarkers();
+
+    LatLngBounds _bounds = FindBoundsCoordinates().getBounds(markersList);
+
+    controller.animateCamera(CameraUpdate.newLatLngBounds(_bounds, 100.0));
   }
 }
 
-Widget _googleMaps(BuildContext context) {
+class FindBoundsCoordinates {
+  LatLngBounds getBounds(Set<MapMarker> locations) {
+    List<double> latitudes = [];
+    List<double> longitude = [];
 
-  showMapSheet(LatLng coordinates, String name, int page) {
-    return showModalBottomSheet(
-      context: context,
-      backgroundColor: Colors.transparent,
-      builder: (BuildContext context){
-        return Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Container(
-              height: 200,
-              child: Expanded(child: MapSheet(coordinates: coordinates, name: name, page: page,)),
-            ),
-          ),
-        );
-      },
+    locations.toList().forEach((index) {
+      latitudes.add(index.position.latitude);
+      longitude.add(index.position.longitude);
+    });
+
+    return LatLngBounds(
+      southwest: LatLng(latitudes.reduce(min), longitude.reduce(min)),
+      northeast: LatLng(latitudes.reduce(max), longitude.reduce(max)),
     );
   }
-
-  List<Marker> religionList = [
-    Marker(
-      markerId: MarkerId("1"),
-      position: LatLng(60.200121, 29.688443),
-      infoWindow: InfoWindow(title: "Храм Покрова Божией матери"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("2"),
-      position: LatLng(60.103617, 29.971210),
-      infoWindow: InfoWindow(title: "Храм святых апостолов Петра и Павла"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("3"),
-      position: LatLng(60.171635, 29.869983),
-      infoWindow: InfoWindow(title: "Храм-часовня Преображения Господня"),
-      icon: religionIcon,
-    ),
-    Marker(
-        markerId: MarkerId("4"),
-        position: LatLng(60.192398, 29.706222),
-        infoWindow: InfoWindow(title: "Храм Казанской иконы Божией матери"),
-        icon: religionIcon,
-        onTap: ()=> Navigator.push(context,
-          MaterialPageRoute(
-            builder: (_) => PlacePage(),
-          ),
-        ),
-    ),
-    Marker(
-      markerId: MarkerId("5"),
-      position: LatLng(60.125738, 30.160218),
-      infoWindow: InfoWindow(title: "Храм Серафима Саровского"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("6"),
-      position: LatLng(60.184690, 29.813569),
-      infoWindow: InfoWindow(title: "Церковь Святого духа и новая церковь"),
-      icon: religionIcon,
-      onTap: ()=> Navigator.push(context,
-        MaterialPageRoute(
-          builder: (_) => PlacePage(),
-        ),
-      ),
-    ),
-    Marker(
-      markerId: MarkerId("7"),
-      position: LatLng(60.123718, 30.122091),
-      infoWindow: InfoWindow(title: "Церковь Петра и Павла"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("8"),
-      position: LatLng(60.101304, 30.209253),
-      infoWindow: InfoWindow(title: "Церковь Святого Николая Чудотворца"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("9"),
-      position: LatLng(60.195830, 29.706692),
-      infoWindow: InfoWindow(title: "Лютеранская Кирха Преображения Господня"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("10"),
-      position: LatLng(60.125417, 30.147307),
-      infoWindow: InfoWindow(title: "Часовня Серафима Саровского"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("11"),
-      position: LatLng(60.089561, 29.965577),
-      infoWindow: InfoWindow(title: "Часовня Николая Чудотворца"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("12"),
-      position: LatLng(60.196749, 29.531765),
-      infoWindow: InfoWindow(title: "Могила Крестовской в поселке Молодежное. Марьина гора"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("13"),
-      position: LatLng(60.213689, 29.730898),
-      infoWindow: InfoWindow(title: "Зеленогорское кладбище. Могила Герценштейна"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("14"),
-      position: LatLng(60.095171, 30.190954),
-      infoWindow: InfoWindow(title: "Левашовское мемориальное кладбище"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("15"),
-      position: LatLng(60.034729, 29.986544),
-      infoWindow: InfoWindow(title: "Кладбище в Горской"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("16"),
-      position: LatLng(60.066926, 29.974975),
-      infoWindow: InfoWindow(title: "Тарховское кладбище"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("17"),
-      position: LatLng(60.203942, 29.799603),
-      infoWindow: InfoWindow(title: "Комаровское кладбище"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("18"),
-      position: LatLng(60.108471, 29.971036),
-      infoWindow: InfoWindow(title: "Сестрорецкое кладбище"),
-      icon: religionIcon,
-    ),
-    Marker(
-      markerId: MarkerId("19"),
-      position: LatLng(60.204525, 29.799806),
-      infoWindow: InfoWindow(title: "Могила Анны Ахматовой"),
-      icon: religionIcon,
-    ),
-  ];
-  List<Marker> militaryList = [
-    Marker(
-      markerId: MarkerId("1"),
-      position: LatLng(60.030205, 30.015883),
-      infoWindow: InfoWindow(title: "Аэродром в Горской"),
-      icon: militaryIcon,
-    ),
-    Marker(
-      markerId: MarkerId("2"),
-      position: LatLng(60.090518, 30.195451),
-      infoWindow: InfoWindow(title: "Военный аэродром в Левашово"),
-      icon: militaryIcon,
-    ),
-    Marker(
-      markerId: MarkerId("3"),
-      position: LatLng(60.099072, 30.213370),
-      infoWindow: InfoWindow(
-          title: "Военный мемориал и памятник дважды Герою СССР А. Т. Карпова",
-          snippet: 'Военнный объект'
-      ),
-      icon: militaryIcon,
-      onTap:(){ showMapSheet(LatLng(60.099072, 30.213370), 'Военный мемориал и памятник дважды Герою СССР А. Т. Карпова', 210);}
-    ),
-    Marker(
-      markerId: MarkerId("4"),
-      position: LatLng(60.184712, 29.818013),
-      infoWindow: InfoWindow(
-          title: "Братская могила воинов Советской Армии/погибших "
-          "в боях Советско-финской и Великой Отечественной войны 1941-1945гг",
-        snippet: 'military'
-      ),
-      icon: militaryIcon,
-      onTap: () {
-        return showModalBottomSheet(
-          context: context,
-          backgroundColor: Colors.transparent,
-          builder: (BuildContext context){
-            return Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                child: Container(
-                  height: 200,
-                  child: MapSheet(name: 'Братская могила воинов Советской Армии/погибших в боях Советско-финской и Великой Отечественной войны 1941-1945гг', page: 5, coordinates: LatLng(60.184712, 29.818013),),
-                ),
-              ),
-            );
-          },
-        );
-      }
-    ),
-  ];
-
-  markersView() {
-      return Set<Marker>.of(
-          museumList
-              + religionList
-              + militaryList
-      );
-  }
-
-
-  return Container(
-    height: MediaQuery.of(context).size.height,
-    width: MediaQuery.of(context).size.width,
-    child: GoogleMap(
-      compassEnabled: true,
-      myLocationButtonEnabled: true,
-      myLocationEnabled: true,
-      mapType: MapType.normal,
-      initialCameraPosition:
-      CameraPosition(target: LatLng(60.1000, 29.5400), zoom: 10),
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-      //markers: {m1, m2, m3},
-      markers: markersView(),
-    ),
-  );
 }
