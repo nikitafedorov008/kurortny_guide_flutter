e957d71b5f14e8a02eb0ab73c5cc46fc0a9ad6ba
Иван Балдин <46814470+IvanBaldin@users.noreply.github.com>
2021-04-05 13:13:49 +0300

    Update MainActivity.kt
    проблема безопасности

--- android/app/src/main/kotlin/com/example/kurortny_guide_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kurortny_guide_flutter/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.kurortny_guide_flutter
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+  // TODO: разлогинься с моего аккаунта, а то портить репозитории буду
 }
